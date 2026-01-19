90148ca486ef2098ad2381cdf7b667330f7311ff
Andre ROSA <csrosa.ex@bancomontepio.pt>
2026-01-19 17:57:24 +0000

    feat: added  hook

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,22 @@
 import type React from 'react';
 
+import { useColorSchemeFavicon } from '@/hooks/useColorSchemeFavicon';
 import { tw } from '@/styles/twind';
 
 import '@/styles/index.css';
 
 export const App: React.FC = () => {
+  useColorSchemeFavicon({
+    faviconDark: './favicon-dark.svg',
+    faviconLight: './favicon-light.svg',
+    onDarkModeEnabled: (event: MediaQueryListEvent) => {
+      console.log("Color scheme changed to dark mode!", event);
+    },
+    onLightModeEnabled: (event: MediaQueryListEvent) => {
+      console.log("Color scheme changed to light mode!", event);
+    },
+  });
+
   return (
     <div className={tw`flex flex-col items-center justify-center min-h-screen`}>
       <h1 className={tw`text-6xl mb-8`}>React application</h1>
